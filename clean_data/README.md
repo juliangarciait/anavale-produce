c11349e79ea4ccad2d5a10f764ba501b059519e2
juliangarciait <46587785+juliangarciait@users.noreply.github.com>
2020-03-25 19:39:17 -0400

    [ADD] app clean_data

---
+++ clean_data/README.md
@@ -0,0 +1,19 @@
+Mass Clean Data
+-------------------------------------------------
+
+Odoo Version : Odoo 13.0 Community
+
+
+Installation 
+-------------------------------------------------------------
+Install the Application => Apps -> Mass Clear Data(clean_data)
+
+
+Overview
+-------------------------------------------------------------
+* Settings > Clean Data > Clean Data.
+* This module will allows users clear multiple objects records from the wizard.
+
+13.0.0.0.1
+-------------------------------------------------------------
+-Improve code and resolve dependency's issue.
---
+++ clean_data/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Mass Clean Data (Clear Data)",
+    'summary': 'This module allows to user clear the unwanted data using wizard',
+    'author': "Aktiv Software",
+    'website': "http://www.aktivsoftware.com",
+    'description': "User can easily clean the data",
+    'category': 'Tools',
+    'version': '13.0.0.0.1',
+    'license': 'AGPL-3',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'wizards/clean_data_view.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'auto_install': False,
+}
