91b967ae27d8f7d2699eaf96f3e86d30b2caf035
Julian Garcia <julian.garcia.it@gmail.com>
2020-04-12 14:39:51 -0600

    mai_add_lotesvivos

--- mai_sale_order_lot_selection/model/sale.py
+++ mai_sale_order_lot_selection/model/sale.py
@@ -11,3 +11,24 @@ class SaleOrderLine(models.Model):
         res = super(SaleOrderLine, self)._prepare_procurement_values(group_id=group_id)
         res['lot_id'] = self.lot_id.id
         return res
+
+
+#apartir de aqui agrego de mi colecta
+
+#solo lotes con qty
+    @api.onchange('product_id')
+    def _onchange_product_id_set_lot_domain(self):
+        available_lot_ids = []
+        if self.order_id.warehouse_id and self.product_id:
+            location = self.order_id.warehouse_id.lot_stock_id
+            quants = self.env['stock.quant'].read_group([
+                ('product_id', '=', self.product_id.id),
+                ('location_id', 'child_of', location.id),
+                ('quantity', '>', 0),
+                ('lot_id', '!=', False),
+            ], ['lot_id'], 'lot_id')
+            available_lot_ids = [quant['lot_id'][0] for quant in quants]
+        self.lot_id = False
+        return {
+            'domain': {'lot_id': [('id', 'in', available_lot_ids)]}
+        }
