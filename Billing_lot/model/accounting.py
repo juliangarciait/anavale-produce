6bdce3d8d032070dfad3b6fe87c420848d1fe359
Julian Garcia <julian.garcia.it@gmail.com>
2020-04-14 18:25:36 -0600

    Revert "billing_lot"
    This reverts commit a0021fdeee698ff5085d05a51e988436b168db60.

--- Billing_lot/model/accounting.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models
-
-
-class BillLot(models.Model):
-    _inherit = 'account.move'
-
-    LotId = fields.Selection(related='stock.production.lot', string="Asignar a Lote", store=True)
-
