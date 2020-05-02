7fb87508d1d2c9bcf5d2036a7a527066819aa3a1
Julian Garcia <julian.garcia.it@gmail.com>
2020-05-02 14:41:52 -0600

    cancel sale orders

---
+++ cancel_all_orders_app/models/sale_order_inherit.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+
+class sale_order(models.Model):
+    _inherit = 'sale.order'
+
+
+    def action_cancel(self):
+        for picking in self.picking_ids:
+            if picking.state != 'cancel':
+                picking.cancel_stock_picking()
+
+        for invoice in self.invoice_ids :
+            if invoice.state != 'cancel':
+                invoice.button_draft()
+                invoice.button_cancel()
+                invoice.update({'name': '/'})
+        res = super(sale_order, self).action_cancel()
+        return res
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
