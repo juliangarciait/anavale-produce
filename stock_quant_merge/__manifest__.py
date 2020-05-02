a372a9a4b8dfde9c9b9fb9a39fd29e6938b541fc
Julian Garcia <julian.garcia.it@gmail.com>
2020-05-02 15:01:14 -0600

    quants merge

---
+++ stock_quant_merge/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2015 OdooMRP team
+# © 2015 AvanzOSC
+# © 2015 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# © 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock - Quant merge",
+    "version": "10.0.1.0.1",
+    "depends": [
+        "stock",
+    ],
+    "author": "OdooMRP team,"
+              "AvanzOSC,"
+              "Tecnativa,"
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "category": "Warehouse",
+    'installable': True,
+    "license": "AGPL-3",
+}
