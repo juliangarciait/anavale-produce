bda3633cb9825f4cd99bead90f5af21d8b883c6b
Mayte Montano <mayte@eadminpro.com>
2020-05-04 19:13:22 -0600

    debug 18

--- anavale/model/stock.py
+++ anavale/model/stock.py
@@ -102,6 +102,6 @@ class StockMove(models.Model):
     def _action_assign(self):
         raise UserError("FILTERED %s" % self.filtered(lambda m: m.state in ['confirmed', 'waiting', 'partially_available']))
     
-        res = super()._action_assign()
+        res = super(StockMove, self)._action_assign()
         
         return res
