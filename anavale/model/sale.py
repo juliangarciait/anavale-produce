07366c2dc05610d1d995ba9b99391c0a489cdd9d
Mayte Montano <mayte@eadminpro.com>
2020-05-04 18:40:20 -0600

    debug

--- anavale/model/sale.py
+++ anavale/model/sale.py
@@ -28,62 +28,55 @@ class SaleOrder(models.Model):
             # if line.product_uom_qty > res['quantity']:
                 # raise UserError('Maximum %s units for selected Lot for Product %s!' % (res['quantity'], line.product_id.name))
         # return res        
-                  
-    @api.model
+      @api.model
     def get_move_from_line(self, line):
-        move = self.env['stock.move']
+        move = self.env["stock.move"]
         # i create this counter to check lot's univocity on move line
         lot_count = 0
-        for p in line.order_id.picking_ids:
-            for m in p.move_lines:
-                move_line_id = m.move_line_ids.filtered(
-                    lambda line: line.lot_id)
-                if move_line_id and line.lot_id == move_line_id[:1].lot_id:
-                    move = m
-                    lot_count += 1
-                    # if counter is 0 or > 1 means that something goes wrong
-                    if lot_count != 1:
-                        raise UserError(_('Can\'t retrieve lot on stock'))
+        for m in line.order_id.picking_ids.mapped("move_lines"):
+            move_line_id = m.move_line_ids.filtered(lambda line: line.lot_id)
+            if move_line_id and line.lot_id == move_line_id[0].lot_id:
+                move = m
+                lot_count += 1
+                # if counter is 0 or > 1 means that something goes wrong
+                if lot_count != 1:
+                    raise UserError(_("Can't retrieve lot on stock"))
         return move
 
     @api.model
     def _check_move_state(self, line):
+        if self.env.context.get("skip_check_lot_selection_move", False):
+            return True
         if line.lot_id:
             move = self.get_move_from_line(line)
-            if move.state == 'confirmed':
+            if move.state == "confirmed":
                 move._action_assign()
                 move.refresh()
-            if move.state != 'assigned':
-                raise UserError(_('Can\'t reserve products for lot %s') %
-                                line.lot_id.name)
+            if move.state != "assigned":
+                raise UserError(
+                    _("Can't reserve products for lot %s") % line.lot_id.name
+                )
         return True
 
-    # @api.multi
     def action_confirm(self):
-        """ Method for 'Confirm' Button, adds add_chatter_autofollow=False
-            so res.partner is not added as follower to the chatter, also 
-            makes sure lot still available before confirming."""
-        res = super(SaleOrder, self.with_context(sol_lot_id=True,add_chatter_autofollow=False))\
-            .action_confirm()
+        res = super(SaleOrder, self.with_context(sol_lot_id=True)).action_confirm()
+        self._check_related_moves()
+        return res
+
+    def _check_related_moves(self):
+        if self.env.context.get("skip_check_lot_selection_qty", False):
+            return True
         for line in self.order_line:
             if line.lot_id:
                 unreserved_moves = line.move_ids.filtered(
-                    lambda move: move.product_uom_qty !=
-                    move.reserved_availability
+                    lambda move: move.product_uom_qty != move.reserved_availability
                 )
                 if unreserved_moves:
                     raise UserError(
-                        _('Can\'t reserve products for lot %s [%s unreserved_moves=%s lot=%s]')
-                        % (line.lot_id.name, line.move_ids,unreserved_moves,unreserved_moves[0].lot_id.name )
+                        _("Can't reserve products for lot %s") % line.lot_id.name
                     )
             self._check_move_state(line)
-            
-        for line in self.mapped('order_line').filtered(lambda line: line.lot_id):
-            # Get avail for this lot ommiting this sale.order.line
-            res = line._get_lots(line.lot_id.id, sale_order_line=line.id)
-            if line.product_uom_qty > res['quantity']:
-                raise UserError('Maximum %s units for selected Lot for Product %s!' % (res['quantity'], line.product_id.name))
-        return res
+        return True
         
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
--- anavale/model/stock.py
+++ anavale/model/stock.py
@@ -68,20 +68,32 @@ class StockMove(models.Model):
                     # line.qty_done = line.product_uom_qty #rec.product_uom_qty
         # return res
 
-    def _update_reserved_quantity(self, need, available_quantity,
-                                  location_id, lot_id=None,
-                                  package_id=None, owner_id=None,
-                                  strict=True):
-        if self._context.get('sol_lot_id'):
+    def _update_reserved_quantity(
+        self,
+        need,
+        available_quantity,
+        location_id,
+        lot_id=None,
+        package_id=None,
+        owner_id=None,
+        strict=True,
+    ):
+        if self._context.get("sol_lot_id"):
             lot_id = self.sale_line_id.lot_id
-        return super(StockMove, self)._update_reserved_quantity(
-            need, available_quantity, location_id, lot_id=lot_id,
-            package_id=package_id, owner_id=owner_id, strict=strict)
+        return super()._update_reserved_quantity(
+            need,
+            available_quantity,
+            location_id,
+            lot_id=lot_id,
+            package_id=package_id,
+            owner_id=owner_id,
+            strict=strict,
+        )
 
     def _prepare_move_line_vals(self, quantity=None, reserved_quant=None):
-        vals = super(StockMove, self)._prepare_move_line_vals(
-            quantity=quantity, reserved_quant=reserved_quant)
-        lot = self.sale_line_id.lot_id
-        if reserved_quant and lot:
-            vals['lot_id'] = lot.id
+        vals = super()._prepare_move_line_vals(
+            quantity=quantity, reserved_quant=reserved_quant
+        )
+        if reserved_quant and self.sale_line_id:
+            vals["lot_id"] = self.sale_line_id.lot_id.id
         return vals
