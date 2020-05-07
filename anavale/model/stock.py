2b72fc7ed4c0c33ed4433fabf558fa420a112db5
Julian Garcia <julian.garcia.it@gmail.com>
2020-05-07 14:18:47 -0600

    fix internal transfer auto details

--- anavale/model/stock.py
+++ anavale/model/stock.py
@@ -81,11 +81,14 @@ class StockMove(models.Model):
         owner_id=None,
         strict=True,
     ):
+        if self._context.get('sol_lot_id'):
+            lot_id = self.sale_line_id.lot_id
+            
         return super()._update_reserved_quantity(
             need,
             available_quantity,
             location_id,
-            lot_id=self.sale_line_id.lot_id,
+            lot_id=lot_id,
             package_id=package_id,
             owner_id=owner_id,
             strict=strict,
