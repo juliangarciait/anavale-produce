7cbb9359aae4844f10d3221f373a9fbeb347a268
Julian Garcia <julian.garcia.it@gmail.com>
2020-05-10 22:35:53 -0600

    debug res.parthner5

--- anavale/model/product_template.py
+++ anavale/model/product_template.py
@@ -4,7 +4,6 @@ from odoo import api, fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
     
-    lot_code_prefix = fields.Char()
     #lot_code_prefix = fields.Char('Lot Code', help='Code used to compute automatic Lot Numbers. 2 digits.', size=2)
     
     #_sql_constraints = [
