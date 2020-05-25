b77149c32758b923a7cab739d8b88090fba9080d
Julian Garcia <julian.garcia.it@gmail.com>
2020-05-25 16:06:19 -0600

    inventory report customization sale orders

--- anavale/model/stock_quant.py
+++ anavale/model/stock_quant.py
@@ -17,21 +17,48 @@ class StockQuant(models.Model):
     def _compute_sale_order_qty(self):
         for quant in self.sudo():        
             domain = [('product_id', '=', quant.product_id.id),
-                ('qty_to_deliver', '>', 0),
+                # ('qty_to_deliver', '>', 0),
                 ('order_id.state', '=', 'sale'),
                 ('lot_id', '=', quant.lot_id.id)]
                 
-            quant.sale_order_quantity = 0    
+            sale_order_quantity = 0    
             for so in self.env['sale.order.line'].search(domain):
-                quant.sale_order_quantity += so._compute_real_qty_to_deliver() 
+                sale_order_quantity += so._compute_real_qty_to_deliver() 
                 # sol._compute_qty_delivered()
-            quant.available_quantity = quant.quantity - quant.sale_order_quantity
+            # quant.available_quantity = quant.quantity - quant.sale_order_quantity
             #     sale_order_quantity += sol.product_uom_qty - sol.qty_delivered
-            # available_quantity = quant.quantity - sale_order_quantity
-            # quant.write({'sale_order_quantity': sale_order_quantity, 'available_quantity': available_quantity})
+            available_quantity = quant.quantity - sale_order_quantity
+            quant.write({'sale_order_quantity': sale_order_quantity, 'available_quantity': available_quantity})
 
     @api.model
     def _quant_tasks(self):
         res = super(StockQuant, self)._quant_tasks()
         self.sudo().search([])._compute_sale_order_qty()
         return res
+
+    def call_view_sale_order(self):
+        """ Method called when click button
+            "View Sale Order" from stock.quant
+            Tree view.
+            Displays Tree view of all sale.order
+            composing self.sale_order_quantity """
+        self.ensure_one()             
+        domain = [('product_id', '=', self.product_id.id),
+            ('order_id.state', '=', 'sale'),
+            ('lot_id', '=', self.lot_id.id)]
+            
+        ids =[]
+        for sol in self.env['sale.order.line'].search(domain):
+            # Only sale.order.line with pending deliveries
+            if sol._compute_real_qty_to_deliver() > 0:
+                ids.append(sol.order_id.id)
+                
+        return  {
+            'type': 'ir.actions.act_window',
+            'name': 'Sale Orders Lot %s' % self.lot_id.name,
+            'res_model': 'sale.order',
+            'view_mode': 'tree,form',
+            'domain': [('id', 'in', ids)],
+            'context': dict(self.env.context),
+            'target': 'self',
+        }
