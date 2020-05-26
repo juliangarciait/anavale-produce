5ae592e80fd886efb7a9b93b8fb0b855d4e9af78
Julian Garcia <julian.garcia.it@gmail.com>
2020-05-25 23:40:00 -0600

    add account analytic on write account.move.line

--- lot_account_tags/model/account_move.py
+++ lot_account_tags/model/account_move.py
@@ -10,3 +10,10 @@ class AccountMoveLine(models.Model):
             #domain = [('name', '=', self.lot_id.account_tag_id)]
             #res = self.env['account.analytic.tag'].search(domain)
             self.analytic_account_id = self.product_id.product_tmpl_id.analytic_account_id
+
+    @api.model
+    def write(self, vals):
+        rec = super(AccountMoveLine, self).write(vals)
+        for line in rec:
+            if line.product_id:
+                line.analytic_account_id = line.product_id.product_tmpl_id.analytic_account_id
