d7c2db51cf53f2b75f9c4447202afddcfdfe02df
Julian Garcia <julian.garcia.it@gmail.com>
2020-05-31 19:04:00 -0600

    validacion cantidad al confirmar

--- anavale/model/__init__.py
+++ anavale/model/__init__.py
@@ -4,4 +4,5 @@ from . import res_partner
 from . import stock
 from . import stock_quant
 from . import stock_quality
+from . import stock_quality_point
 from . import mail_thread
--- anavale/model/stock_quality.py
+++ anavale/model/stock_quality.py
@@ -9,8 +9,8 @@ class StockQualityCheck(models.Model):
     picking_id = fields.Many2one('stock.picking', 'Picking', readonly=True)  
     product_id = fields.Many2one(
         'product.product', 'Product',
-        domain="[('type', 'in', ['consu', 'product'])]", required=True)
-    lot_id = fields.Many2one('stock.production.lot', 'Lot', domain="[('product_id', '=', product_id)]", required=True)    
+        domain="[('type', 'in', ['consu', 'product'])]")
+    lot_id = fields.Many2one('stock.production.lot', 'Lot', domain="[('product_id', '=', product_id)]")    
     responsible_id = fields.Char('Responsible')
     partner_id = fields.Many2one('res.partner', string='Vendor', readonly=True)
     quality_lines = fields.One2many('stock.quality.check.line', 'quality_id', string="Quality Lines", copy=False)
@@ -50,67 +50,48 @@ class StockQualityCheck(models.Model):
             
         return {
             'domain': {'lot_id': [('id', 'in', lot_ids)] }
-        }    
-            
-        # # Create Quality lines if necessary
-        # quality_lines = []
-        # from odoo.exceptions import UserError
-        # # raise UserError('picking_id.move_line_ids_without_package [%s]' % picking_id.move_line_ids_without_package)
-        # for line in picking_id.move_line_ids_without_package:
-            # order_line_values = {
-                # 'product_id': line.product_id.id,
-                # 'lot_id': line.lot_id.id
-            # }
-            # quality_lines.append((0, 0, order_line_values))
-        # self.quality_lines = quality_lines
-        # raise UserError('quality_lines [%s]' % quality_lines)
+        }              
         
 class StockQualityCheckLine(models.Model):
     _name = 'stock.quality.check.line'
     _description = "Stock Quality Control Line"
-    
+        
+    brand = fields.Char('Brand/Product')      
     quality_id = fields.Many2one('stock.quality.check', 'Quality Control Reference', index=True)  
     weight = fields.Float(string='Weight', digits='Product Unit of Measure', default=0.0)
     count = fields.Integer(string='Count')    
-    # size = fields.Selection([
-        # ('xl', 'XL'),
-        # ('jb', 'JB'),
-        # ('lg', 'LG')], string='Size') 
-    damage = fields.Percent(string='Damage (%)') 
-    insect = fields.Percent(string='Insect (%)') 
-    decay = fields.Percent(string='Decay (%)') 
-    stripes = fields.Percent(string='Stripes (%)') 
-    red = fields.Percent(string='Red (%)') 
-    firmness = fields.Selection([
-        ('1', 'Flacid'),
-        ('2', 'Soft'),
-        ('3', 'Firm'),
-        ('4', 'Hard')], string='Firmness',)  
-        
-        
-        
-        
-     # @api.onchange('picking_id')
-    # def _onchange_picking_id(self):
-        # if not self.picking_id:
-            # return
+    point_ids = fields.One2many('stock.quality.check.point', 'line_id', string="Quality Points")
+    size = fields.Selection([
+        ('xl', 'XL'),
+        ('jb', 'JB'),
+        ('lg', 'LG')], string='Size')         
 
-        # picking_id = self.picking_id
-        # if self.partner_id:
-            # partner = self.partner_id
-        # else:
-            # partner = picking_id.partner_id
-        # self.partner_id = partner.id
-        # # Create Quality lines if necessary
-        # quality_lines = []
-        # from odoo.exceptions import UserError
-        # # raise UserError('picking_id.move_line_ids_without_package [%s]' % picking_id.move_line_ids_without_package)
-        # for line in picking_id.move_line_ids_without_package:
-            # order_line_values = {
-                # 'product_id': line.product_id.id,
-                # 'lot_id': line.lot_id.id
-            # }
-            # quality_lines.append((0, 0, order_line_values))
-        # self.quality_lines = quality_lines
-        # raise UserError('quality_lines [%s]' % quality_lines)
+    def action_open_quality_points(self):
+        """ Opens Quality Points.
+        """
+        view = self.env.ref('anavale.view_stock_quality_line_form')    
+        return {
+            'name': 'Stock Quality Points',
+            'res_model':'stock.quality.check.line',
+            'res_id': self.id,
+            'views': [[view.id, "form"]],
+            'view_mode': 'self',
+            'type': 'ir.actions.act_window',
+            'contex': dict(self.env.context),
+            'target': 'new',
+        }
         
+class StockQualityCheckPoint(models.Model):
+    _name = 'stock.quality.check.point'
+    _description = "Stock Quality Control Check Points"
+    
+    line_id = fields.Many2one('stock.quality.check.line', 'Quality Line Reference')  
+    point_id = fields.Many2one('stock.quality.point', string='Name', ondelete='restrict')
+    value = fields.Integer(string='Value')  
+    percentaje = fields.Percent(string='Percentaje')   
+        
+    @api.onchange('value')
+    def _onchange_picking_id(self):
+        for record in self:
+            if record.value and record.line_id.count > 0:
+                record.percentaje = int((record.value * 100) / record.line_id.count)
