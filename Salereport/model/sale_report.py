c9ee8d7e5af8c7d8f296160a2bc741870086de67
Luis Felipe <felipe23_23@hotmail.com>
2020-10-01 00:58:59 -0500

    Fix on sales report AVG price

--- Salereport/model/sale_report.py
+++ Salereport/model/sale_report.py
@@ -17,10 +17,21 @@ class SaleReport(models.Model):
     #     for line in self:
     #         res=line
 
+    """
+    fields['avg_price'] = ", ((sum(l.price_total / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE " \
+                              "s.currency_rate END))/CASE (sum(l.qty_invoiced / u.factor * u2.factor)) WHEN 0 THEN " \
+                              "1.0 ELSE  (sum(l.qty_invoiced / u.factor * u2.factor)) END)  as avg_price "
+    """
+
 
     def _query(self, with_clause='', fields={}, groupby='', from_clause=''):
         fields['lot_id'] = ", lot.name as lot_id"
-        fields['avg_price'] = ", (sum(price_subtotal)/sum(product_uom_qty)) as avg_price"
+        #fields['avg_price'] = ",   (ROUND(sum(price_subtotal),2) / (ROUND((CASE (sum(product_uom_qty)) WHEN 0 THEN " \
+        #                      "1.0 ELSE (sum(product_uom_qty)) END  ),2))) as avg_price "
+
+        fields['avg_price'] = ", ((sum(l.price_total / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE " \
+                              "s.currency_rate END))/CASE (sum(l.qty_invoiced / u.factor * u2.factor)) WHEN 0 THEN " \
+                              "1.0 ELSE  (sum(l.qty_invoiced / u.factor * u2.factor)) END)  as avg_price "
         groupby += ', lot.name'
         from_clause = ' left join stock_production_lot lot on (l.lot_id=lot.id)'
         return super(SaleReport, self)._query(with_clause, fields, groupby, from_clause)
