5c8c455b1e76e503852dc90a029d16c8cff675b6
Julian <julian.garcia.it@gmail.com>
2021-04-02 17:06:40 -0600

    agegar credit limit customers

---
+++ partner_credit_limit/models/sale.py
@@ -0,0 +1,57 @@
+# See LICENSE file for full copyright and licensing details.
+
+
+from odoo import api, models, _
+from odoo.exceptions import UserError
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def check_limit(self):
+        self.ensure_one()
+        partner = self.partner_id
+        user_id = self.env['res.users'].search([
+            ('partner_id', '=', partner.id)], limit=1)
+        if user_id and not user_id.has_group('base.group_portal') or not \
+                user_id:
+            moveline_obj = self.env['account.move.line']
+            movelines = moveline_obj.search(
+                [('partner_id', '=', partner.id),
+                 ('account_id.user_type_id.name', 'in',
+                  ['Receivable', 'Payable'])]
+            )
+            confirm_sale_order = self.search([('partner_id', '=', partner.id),
+                                              ('state', '=', 'sale')])
+            debit, credit = 0.0, 0.0
+            amount_total = 0.0
+            for status in confirm_sale_order:
+                amount_total += status.amount_total
+            for line in movelines:
+                credit += line.credit
+                debit += line.debit
+            partner_credit_limit = (partner.credit_limit - debit) + credit
+            available_credit_limit = \
+                (partner_credit_limit - debit)
+            if (amount_total - debit) > available_credit_limit:
+                if not partner.over_credit:
+                    msg = 'Your available credit limit' \
+                          ' Amount = %s \nCheck "%s" Accounts or Credit ' \
+                          'Limits.' % (partner.credit_limit,
+                                       self.partner_id.name)
+                    raise UserError(_('You can not confirm Sale '
+                                      'Order. \n' + msg))
+                # partner.write(
+                #     {'credit_limit': credit - debit + self.amount_total})
+            return True
+
+    def action_confirm(self):
+        res = super(SaleOrder, self).action_confirm()
+        for order in self:
+            order.check_limit()
+        return res
+
+    @api.constrains('amount_total')
+    def check_amount(self):
+        for order in self:
+            order.check_limit()
