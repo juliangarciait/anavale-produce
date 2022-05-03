3a737ce7fe4e6266ce86e7de181be9852f7aa6ca
Julian <julian.garcia.it@gmail.com>
2022-05-03 15:50:01 -0500

    correccion manifest

--- anavale/__manifest__.py
+++ anavale/__manifest__.py
@@ -37,7 +37,7 @@
             'report/report_quality.xml',
             'report/sale_report_avg_views.xml',
             'report/sale_report_by_lot_views.xml',
-            'views/account_move_view.xml'
+            'views/account_move_view.xml',
             'views/stock_picking_view.xml',
             'report/delivery_slip_views.xml',
              ],
