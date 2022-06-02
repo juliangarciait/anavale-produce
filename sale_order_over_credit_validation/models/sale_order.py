be5c11c91aaff61c9ac533cf4cc92a371874a37a
ArmandoBeltran <beltran.armando2210@gmail.com>
2022-06-02 13:53:27 -0500

    Cambio en sintaxis de la validacion

--- sale_order_over_credit_validation/models/sale_order.py
+++ sale_order_over_credit_validation/models/sale_order.py
@@ -10,6 +10,6 @@ class SaleOrder(models.Model):
     def action_confirm(self): 
         today = datetime.now().date()
         inv_ids = self.env['account.move'].search([('partner_id', '=', self.partner_id.id), ('state', '=', 'posted'), ('invoice_payment_state', '=', 'not_paid'), ('type', '=', 'out_invoice'), ('invoice_date_due', '<', today)])
-        if inv_ids and self.partner_id.over_credit == False:
+        if inv_ids and not self.partner_id.over_credit:
             raise ValidationError (_("You can not confirm sale order for this Customer. This customer has one or more overdue invoices."))
         return super(SaleOrder, self).action_confirm()
