e88b0f6784d11f655ce3d56b0037d4b4974d2551
Julian <julian.garcia.it@gmail.com>
2022-08-16 12:32:42 -0500

    agregar cambio de la formula check en sale order

--- partner_credit_limit/models/partner.py
+++ partner_credit_limit/models/partner.py
@@ -9,11 +9,44 @@ class ResPartner(models.Model):
     over_credit = fields.Boolean('Allow Over Credit?')
     credit_insured = fields.Float(string='Asegurado')
     credit_manual = fields.Float(string='Manual')
+    credit_available = fields.Float(string='Credit Available',compute='_compute_credit_available')
 
     @api.onchange('credit_insured', 'credit_manual')
     def onchange_credito(self):
         self.credit_limit = self.credit_insured
         if self.credit_manual > self.credit_insured:
             self.credit_limit = self.credit_manual
+
+    def _compute_credit_available(self):
+        self.ensure_one()
+        partner = self
+        user_id = self.env['res.users'].sudo().search([
+            ('partner_id', '=', partner.id)], limit=1)
+        if user_id and not user_id.has_group('base.group_portal') or not \
+                user_id:
+            moveline_obj = self.env['account.move'].sudo()
+            movelines = moveline_obj.search(
+                [('partner_id', '=', partner.id),
+                 ('state', '=', 'posted'),
+                 ('type', '=', 'out_invoice')]
+            )
+            confirm_sale_order = self.env['sale.order'].sudo().search([('partner_id', '=', self.id),
+                                              ('invoice_status', '=', 'to invoice')])
+            confirm_sale_order_delivery = self.env['sale.order'].sudo().search([('partner_id', '=', partner.id),
+                                              ('custom_state_delivery', '=', 'Waiting')])
+            debit, credit = 0.0, 0.0
+            due = 0
+            amount_total = 0.0
+            for status in confirm_sale_order:
+                amount_total += status.amount_total
+            for status in confirm_sale_order_delivery:
+                amount_total += status.amount_total
+            self.credit_limit = self.credit_insured
+            if self.credit_manual > self.credit_insured:
+                self.credit_limit = self.credit_manual
+            for line in movelines:
+                due += line.amount_residual_signed
+            self.credit_available = self.credit_limit - due - amount_total
+
 
 
--- partner_credit_limit/models/sale.py
+++ partner_credit_limit/models/sale.py
@@ -11,6 +11,51 @@ class SaleOrder(models.Model):
     def check_limit(self):
         self.ensure_one()
         partner = self.partner_id
+        user_id = self.env['res.users'].sudo().search([
+            ('partner_id', '=', partner.id)], limit=1)
+        if user_id and not user_id.has_group('base.group_portal') or not \
+                user_id:
+            moveline_obj = self.env['account.move'].sudo()
+            movelines = moveline_obj.search(
+                [('partner_id', '=', partner.id),
+                 ('state', '=', 'posted'),
+                 ('type', '=', 'out_invoice')]
+            )
+            confirm_sale_order = self.env['sale.order'].sudo().search([('partner_id', '=', self.id),
+                                                                       ('invoice_status', '=', 'to invoice')])
+            confirm_sale_order_delivery = self.env['sale.order'].sudo().search([('partner_id', '=', partner.id),
+                                                                                ('custom_state_delivery', '=','Waiting')])
+            debit, credit = 0.0, 0.0
+            due = 0
+            amount_total = 0.0
+            for status in confirm_sale_order:
+                amount_total += status.amount_total
+            for status in confirm_sale_order_delivery:
+                amount_total += status.amount_total
+            credit = partner.credit_insured
+            if partner.credit_insured < partner.credit_manual:
+                credit = partner.credit_manual
+            for line in movelines:
+                due += line.amount_residual_signed
+            credit_used = credit - due - amount_total
+            credit_available = credit_used + self.amount_total
+
+
+            if credit_used < 0:
+                if not partner.over_credit:
+                    msg = 'Your available credit available' \
+                          ' is  $%s \nCheck "%s" Accounts or Credit ' \
+                          'Limits.' % (credit_available,
+                                       self.partner_id.name)
+                    raise UserError(_('You can not confirm Sale '
+                                      'Order. \n' + msg))
+                # partner.write(
+                #     {'credit_limit': credit - debit + self.amount_total})
+            return True
+
+    def check_limit1(self):
+        self.ensure_one()
+        partner = self.partner_id
         user_id = self.env['res.users'].search([
             ('partner_id', '=', partner.id)], limit=1)
         if user_id and not user_id.has_group('base.group_portal') or not \
@@ -56,7 +101,7 @@ class SaleOrder(models.Model):
             order.check_limit()
         return res
 
-    @api.constrains('amount_total')
+    #@api.constrains('amount_total')
     def check_amount(self):
         for order in self:
             order.check_limit()
