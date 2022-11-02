aad02b4adacee34ba4090929ac15b30cf71b93dd
Luis Ernesto García Medina <ernesto.r.2.em@gmail.com>
2022-11-02 13:10:32 -0600

    Se corrige error

--- liquidaciones/wizard/selection_wizard.py
+++ liquidaciones/wizard/selection_wizard.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from dataclasses import Field
-from email.policy import default
 from odoo import fields, models, api
 from odoo.exceptions import UserError
 import logging
