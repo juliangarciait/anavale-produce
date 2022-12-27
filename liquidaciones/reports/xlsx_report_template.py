c56d25b448564182597fb70372d0e654233a885f
ArmandoBeltran <beltran.armando2210@gmail.com>
2022-12-27 14:10:09 -0600

    Se agrega otro formato a la plantilla

--- liquidaciones/reports/xlsx_report_template.py
+++ liquidaciones/reports/xlsx_report_template.py
@@ -25,10 +25,13 @@ class XlsxReport(models.AbstractModel):
         sheet.set_column(4, 0, 25)
         sheet.set_column(4, 1, 25)
         sheet.set_column(8, 9, 20)
+        sheet.set_column(4, 17, 30) 
+        sheet.set_column(4, 18, 20)
         
         report_format = workbook.add_format({
             'font_color' : 'red',
             'font_size'  : '10', 
+            'font_name'  : 'arial',
             'border'     : 2,
             'bg_color'   : 'yellow',
             'bold'       : True,
@@ -36,7 +39,8 @@ class XlsxReport(models.AbstractModel):
         })
         report_format_title = workbook.add_format({
             'font_color' : 'red',
-            'font_size'  : '16', 
+            'font_size'  : '16',
+            'font_name'  : 'arial', 
             'border'     : 2,
             'bg_color'   : 'yellow',
             'bold'       : True,
@@ -45,6 +49,7 @@ class XlsxReport(models.AbstractModel):
         bold_header = workbook.add_format({
             'font_color' : 'red',
             'font_size'  : '14', 
+            'font_name'  : 'arial',
             'bottom'     : 2,
             'bg_color'   : 'yellow',
             'bold'       : True,
@@ -53,6 +58,7 @@ class XlsxReport(models.AbstractModel):
         light_header_top = workbook.add_format({
             'font_color' : 'red',
             'font_size'  : '14', 
+            'font_name'  : 'arial',
             'top'        : 2,
             'right'      : 1,
             'left'       : 1,
@@ -63,6 +69,7 @@ class XlsxReport(models.AbstractModel):
         light_header_bottom = workbook.add_format({
             'font_color' : 'red',
             'font_size'  : '14', 
+            'font_name'  : 'arial',
             'bottom'     : 2,
             'top'        : 1, 
             'right'      : 1,
@@ -73,12 +80,90 @@ class XlsxReport(models.AbstractModel):
         light_box = workbook.add_format({
             'font_color' : 'red',
             'font_size'  : '14', 
+            'font_name'  : 'arial',
             'bottom'     : 1,
             'top'        : 1, 
             'right'      : 1,
             'left'       : 1,
             'bg_color'   : 'yellow',
             'align'      : 'center',
+        })
+        travels = workbook.add_format({
+            'font_color' : 'black',
+            'font_size'  : '14',  
+            'font_name'  : 'arial',
+            'align'      : 'left',
+            'bold'       : True
+        })
+        travels_title_top_left = workbook.add_format({
+            'font_color' : 'black', 
+            'font_size'  : '14', 
+            'font_name'  : 'arial', 
+            'align'      : 'left', 
+            'top'        : 2, 
+            'left'       : 2,
+            'bold'       : True
+        })
+        travels_middle_left = workbook.add_format({
+            'font_color' : 'black',
+            'font_size'  : '14', 
+            'font_name'  : 'arial', 
+            'align'      : 'left', 
+            'left'       : 2,
+            'bold'       : True
+        })
+        travels_bottom_left = workbook.add_format({
+            'font_color' : 'black',
+            'font_size'  : '14', 
+            'font_name'  : 'arial', 
+            'align'      : 'left', 
+            'left'       : 2,
+            'bottom'     : 2,
+            'bold'       : True
+        })
+        travels_middle_left_red = workbook.add_format({
+            'font_color' : 'white',
+            'font_size'  : '14', 
+            'font_name'  : 'arial', 
+            'align'      : 'left', 
+            'left'       : 2, 
+            'bg_color'   : 'red',
+            'bold'       : True
+        })
+        travels_title_top_right = workbook.add_format({
+            'font_color' : 'black', 
+            'font_size'  : '14', 
+            'font_name'  : 'arial', 
+            'align'      : 'right', 
+            'top'        : 2, 
+            'right'      : 2,
+            'bold'       : True
+        })
+        travels_middle_right = workbook.add_format({
+            'font_color' : 'black',
+            'font_size'  : '14', 
+            'font_name'  : 'arial', 
+            'align'      : 'right', 
+            'right'      : 2,
+            'bold'       : True
+        })
+        travels_middle_right_red = workbook.add_format({
+            'font_color' : 'white',
+            'font_size'  : '14', 
+            'font_name'  : 'arial', 
+            'align'      : 'right', 
+            'right'      : 2, 
+            'bg_color'   : 'red',
+            'bold'       : True
+        })
+        travels_bottom_right = workbook.add_format({
+            'font_color' : 'black',
+            'font_size'  : '14', 
+            'font_name'  : 'arial', 
+            'align'      : 'right', 
+            'right'      : 2,
+            'bottom'     : 2,
+            'bold'       : True
         })
         
         sheet.write(4, 0, 'NOTA', report_format)
@@ -144,6 +229,9 @@ class XlsxReport(models.AbstractModel):
         sheet.write(10, 8, '', light_box)
         sheet.write(10, 9, '', light_box)
         sheet.write(10, 10, '', light_box)
+        sheet.write(10, 12, '', light_box)
+        sheet.write(10, 13, '', light_box)
+        sheet.write(10, 14, '', light_box)
         sheet.write(11, 0, '', light_box)
         sheet.write(11, 1, '', light_box)
         sheet.write(11, 2, '', light_box)
@@ -232,8 +320,57 @@ class XlsxReport(models.AbstractModel):
         sheet.write(18, 8, '', light_box)
         sheet.write(18, 9, '', light_box)
         sheet.write(18, 10, '', light_box)
-        
+        sheet.write(19, 0, '', light_box)
+        sheet.write(19, 1, '', light_box)
+        sheet.write(19, 2, '', light_box)
+        sheet.write(19, 3, '', light_box)
+        sheet.write(19, 4, '', light_box)
+        sheet.write(19, 5, '', light_box)
+        sheet.write(19, 6, '', light_box)
+        sheet.write(19, 7, '', light_box)
+        sheet.write(19, 8, '', light_box)
+        sheet.write(19, 9, '', light_box)
+        sheet.write(19, 10, '', light_box)
+
 
         sheet.merge_range(5, 2, 5, 10, 'AGROFRESH', report_format_title)
+        
+        
+        sheet.write(4, 17, 'Viaje', travels)
+        sheet.write(5, 17, 'VENTAS', travels_title_top_left)
+        sheet.write(5, 18, '', travels_title_top_right)
+        
+        sheet.write(6, 17, '', travels_middle_left)
+        sheet.write(7, 17, '', travels_middle_left)
+        sheet.write(8, 17, 'LIQUIDACIONES', travels_middle_left)
+        sheet.write(9, 17, 'Freight In', travels_middle_left)
+        sheet.write(10, 17, 'Aduana', travels_middle_left)
+        sheet.write(15, 17, '', travels_middle_left)
+        sheet.write(16, 17, '', travels_middle_left)
+        sheet.write(17, 17, 'UTILIDAD', travels_middle_left)
+        sheet.write(18, 17, '', travels_middle_left)
+        
+        sheet.write(6, 18, '', travels_middle_right)
+        sheet.write(7, 18, '', travels_middle_right)
+        sheet.write(8, 18, '', travels_middle_right)
+        sheet.write(9, 18, '', travels_middle_right)
+        sheet.write(10, 18, '', travels_middle_right)
+        sheet.write(15, 18, '', travels_middle_right)
+        sheet.write(16, 18, '', travels_middle_right)
+        sheet.write(17, 18, '', travels_middle_right)
+        sheet.write(18, 18, '', travels_middle_right)
+        
+        sheet.write(11, 17, 'MANIOBRAS', travels_middle_left_red)
+        sheet.write(12, 17, 'AJUSTE', travels_middle_left_red)
+        sheet.write(13, 17, 'STORAGE', travels_middle_left_red)
+        sheet.write(14, 17, 'FREIGHT OUT', travels_middle_left_red)
+        sheet.write(11, 18, '', travels_middle_right_red)
+        sheet.write(12, 18, '', travels_middle_right_red)
+        sheet.write(13, 18, '', travels_middle_right_red)
+        sheet.write(14, 18, '', travels_middle_right_red)
+        
+        sheet.write(19, 17, '', travels_bottom_left)
+        sheet.write(19, 18, '', travels_bottom_right)
+        
         
     
