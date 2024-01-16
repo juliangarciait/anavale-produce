72a0e85c19def0484c6c4bfcf875f8acc284acdb
Julian <juliangarcia1@Julians-MacBook-Pro.local>
2024-01-16 07:57:46 -0600

    stock scrap force date done

--- anavale/model/stock_scrap.py
+++ anavale/model/stock_scrap.py
@@ -1,20 +1,32 @@
 from odoo import api, fields, models
+from datetime import timedelta
 
 class StockCrap(models.Model):
     _inherit = "stock.scrap"
 
     tag_ids = fields.Many2many("account.analytic.tag")
+    date_move = fields.Datetime('Move Date')
 
     def action_validate(self):
+        if self.date_move:
+            self.date = self.date_move
         res = super(StockCrap, self).action_validate()
 
     def do_scrap(self):
         self._check_company()
         for scrap in self:
             scrap.name = self.env['ir.sequence'].next_by_code('stock.scrap') or _('New')
-            move = self.env['stock.move'].create(scrap._prepare_move_values())
+            scrap_date = scrap._prepare_move_values()
+            move = self.env['stock.move'].create(scrap_date)
             # master: replace context by cancel_backorder
             move.with_context(is_scrap=True)._action_done()
+            if self.date_move:
+                for m in move:
+                    m.write({ 'date': self.date_move - timedelta(hours=6)})
+                    for line in m.move_line_ids:
+                        line.write({ 'date': self.date_move - timedelta(hours=6)})
+                    for accmove in move.account_move_ids:
+                        accmove.write({ 'date': self.date_move - timedelta(hours=6)})
             if self.tag_ids:
                 for line in move:
                       for acc_move in line.account_move_ids:
@@ -23,5 +35,10 @@ class StockCrap(models.Model):
                                   if not invoice_line.analytic_tag_ids:
                                     invoice_line.analytic_tag_ids = self.tag_ids
             scrap.write({'move_id': move.id, 'state': 'done'})
-            scrap.date_done = fields.Datetime.now()
+            if self.date_move:
+                scrap.date_done = self.date_move
+            else:
+                scrap.date_done = fields.Datetime.now()
         return True
+    
+    
