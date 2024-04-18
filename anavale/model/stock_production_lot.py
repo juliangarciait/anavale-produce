604521422d7f58dc1c9403dc71a66a4ace03830a
Julian <juliangarcia1@Julians-MacBook-Pro.local>
2024-04-18 11:00:32 -0600

    fix recepcion cancelada, fecha de factura

--- anavale/model/stock_production_lot.py
+++ anavale/model/stock_production_lot.py
@@ -9,30 +9,27 @@ class StockProductionLot(models.Model):
     _order = 'create_date DESC'
 
 
-    available_to_choose = fields.Boolean('Available', store=True, compute='_available')
+    available_to_choose = fields.Boolean('Available', store=True)
 
     @api.depends('quant_ids', 'quant_ids.reserved_quantity', 'quant_ids.quantity', 'product_qty')
     def _available(self):
         for lot in self:
             if lot.product_qty <= 0:
                 lot.available_to_choose = False
-            elif lot.product_qty > 0:
-                quants = lot.quant_ids.filtered(lambda q: q.location_id.usage in ['internal', 'transit'])
-                for q in quants:
-                    if q.quantity - q.reserved_quantity > 0:
-                        lot.available_to_choose = True
-                    elif q.quantity - q.reserved_quantity < 0:
-                        lot.available_to_choose = False
-
-    @api.onchange('product_qty')
-    def _onchange_usage(self):
-        if self.product_qty > 0:
-            self.available_to_choose = True
-        else:
-            self.available_to_choose = False
+            else:
+                lot.available_to_choose = True
 
     def _product_qty(self):
         res = super(StockProductionLot, self)._product_qty()
         for lot in self:
             if lot.product_qty > 0:
                 lot.available_to_choose = True
+            else: 
+                lot.available_to_choose = False
+
+    def create(self, vals):
+        res = super(StockProductionLot, self).create(vals)
+        for lot in res:
+            lot.available_to_choose = True
+        return res
+
